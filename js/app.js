0f8b4e8766be167bc18ee7dd23a26d9115301bce
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2016-12-02 17:43:22 -0800

    Add js to style map, generate map with San Francisco centered, add marker locations and add function to place markers.

---
+++ js/app.js
@@ -0,0 +1,91 @@
+var map;
+var markers = []; // Blank array to hold markers
+
+function initMap() { // add timeout
+    var styles = [ // Map color mix of  https://snazzymaps.com/style/30/cobalt and https://snazzymaps.com/style/17/bright-and-bubbly
+        {
+            featureType: "all",
+            elementType: "all",
+            stylers: [{
+                "invert_lightness": true
+            }, {
+                "saturation": 10
+            }, {
+                "lightness": 30
+            }, {
+                "gamma": 0.5
+            }, {
+                "hue": "#435158"
+            }]
+        }, {
+            featureType: 'administrative',
+            elementType: 'labels.text.stroke',
+            stylers: [{
+                color: '#ffffff'
+            }, {
+                weight: 6
+            }]
+        }, {
+            featureType: 'administrative',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#e85113'
+            }]
+        }
+    ];
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 37.7636505,
+            lng: -122.4556266
+        },
+        zoom: 13,
+        styles: styles
+    });
+    var locations = [{
+            title: 'Mechanics Institute Library and Chess Room',
+            location: {
+                lat: 37.788862,
+                lng: -122.402976
+            }
+        }, {
+            title: 'San Francisco Center for the Book',
+            location: {
+                lat: 37.765357,
+                lng: -122.402344
+            }
+        }, {
+            title: 'Letterform Archive',
+            location: {
+                lat: 37.763783,
+                lng: -122.394046
+            }
+        }, {
+            title: 'Golden Gate Park Model Yacht Club',
+            location: {
+                lat: 37.771221,
+                lng: -122.494308
+            }
+        }, {
+            title: 'Book Club of California',
+            location: {
+                lat: 37.789842,
+                lng: -122.405878
+            }
+        }
+
+    ];
+    for (var i = 0; i < locations.length; i++) {
+      debugger;
+      var position = locations[i].location;
+      var title = locations[i].title;
+      var marker = new google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        id: i
+      })
+      markers.push(marker);
+    }
+}
