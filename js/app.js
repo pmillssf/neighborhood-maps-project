d96457721a6368bf2ccb729936db6990b3fdcae8
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2016-12-06 18:33:21 -0800

    Add knockout model organization

--- js/app.js
+++ js/app.js
@@ -1,6 +1,45 @@
+var locations = ko.observableArray([{
+        title: 'Mechanics Institute Library and Chess Room',
+        location: {
+            lat: 37.788862,
+            lng: -122.402976
+        },
+        icon: 'images/2915103_1.png'
+    }, {
+        title: 'San Francisco Center for the Book',
+        location: {
+            lat: 37.765357,
+            lng: -122.402344
+        },
+        icon: 'images/book.png'
+    }, {
+        title: 'Letterform Archive',
+        location: {
+            lat: 37.763783,
+            lng: -122.394046
+        },
+        icon: 'images/schreibwaren_web.png'
+    }, {
+        title: 'Golden Gate Park Model Yacht Club',
+        location: {
+            lat: 37.771221,
+            lng: -122.494308
+        },
+        icon: 'images/sailing.png'
+    }, {
+        title: 'Book Club of California',
+        location: {
+            lat: 37.789842,
+            lng: -122.405878
+        },
+        icon: 'images/library.png'
+    }
+
+]);
+var markers = ko.observableArray([]); // Blank array to hold markers
+
 var map;
 var marker;
-var markers = []; // Blank array to hold markers
 
 function initMap() { // add timeout
     var styles = [ // Map color mix of  https://snazzymaps.com/style/30/cobalt and https://snazzymaps.com/style/17/bright-and-bubbly
@@ -43,82 +82,52 @@ function initMap() { // add timeout
         zoom: 13,
         styles: styles
     });
-    var locations = [{
-            title: 'Mechanics Institute Library and Chess Room',
-            location: {
-                lat: 37.788862,
-                lng: -122.402976
-            },
-            icon: 'images/2915103_1.png'
-        }, {
-            title: 'San Francisco Center for the Book',
-            location: {
-                lat: 37.765357,
-                lng: -122.402344
-            },
-            icon: 'images/book.png'
-        }, {
-            title: 'Letterform Archive',
-            location: {
-                lat: 37.763783,
-                lng: -122.394046
-            },
-            icon: 'images/schreibwaren_web.png'
-        }, {
-            title: 'Golden Gate Park Model Yacht Club',
-            location: {
-                lat: 37.771221,
-                lng: -122.494308
-            },
-            icon: 'images/sailing.png'
-        }, {
-            title: 'Book Club of California',
-            location: {
-                lat: 37.789842,
-                lng: -122.405878
-            },
-            icon: 'images/library.png'
-        }
-
-    ];
-    var infoWindow = new google.maps.InfoWindow();
-    var bounds = new google.maps.LatLngBounds();
-    for (var i = 0; i < locations.length; i++) {
-      var position = locations[i].location;
-      var title = locations[i].title;
-      marker = new google.maps.Marker({
-        map: map,
-        position: position,
-        title: title,
-        icon: locations[i].icon,
-        animation: google.maps.Animation.DROP,
-        id: i
-      })
-      markers.push(marker);
-      marker.addListener('click', function(){
-        populateInfoWindow(this, infoWindow);
-      });
-      //bounds.extend(markers[i].position);
-    }
-    //map.fitBounds(bounds)
+    ko.applyBindings(new MapViewModel());
 }
 
-function populateInfoWindow(marker, infowindow) {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-    infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>');
-    infowindow.open(map, marker);
-    // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick',function(){
-      infowindow.setMarker(null);
+
+function MapViewModel() {
+  var self = this;
+  var infoWindow = new google.maps.InfoWindow();
+  var bounds = new google.maps.LatLngBounds();
+  for (var i = 0; i < locations().length; i++) {
+    var position = locations()[i].location;
+    var title = locations()[i].title;
+    marker = new google.maps.Marker({
+      map: map,
+      position: position,
+      title: title,
+      icon: locations()[i].icon,
+      animation: google.maps.Animation.DROP,
+      id: i
+    })
+    markers.push(marker);
+    console.log(markers());
+    marker.addListener('click', function(){
+      populateInfoWindow(this, infoWindow);
     });
-  };
-  // Add bounce animation to marker on click
-  if (marker.getAnimation() === null) {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-    setTimeout(function() {
-      marker.setAnimation(null);
-    }, 1400);
+    //bounds.extend(markers[i].position);
+  }
+  //map.fitBounds(bounds)
+
+  function populateInfoWindow(marker, infowindow) {
+    debugger;
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent('<div>' + marker.title + '</div>');
+      infowindow.open(map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick',function(){
+        infowindow.setMarker(null);
+      });
+    };
+    // Add bounce animation to marker on click
+    if (marker.getAnimation() === null) {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function() {
+        marker.setAnimation(null);
+      }, 1400);
+    }
   }
 }
